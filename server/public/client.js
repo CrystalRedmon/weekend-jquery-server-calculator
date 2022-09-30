0277583f7b8c92e7a8a77d35e65f72180233a18e
Crystal Redmon <cato2848@gmail.com>
2022-09-29 23:38:02 -0500

    begin ajax POST, app POST

--- server/public/client.js
+++ server/public/client.js
@@ -9,6 +9,8 @@ $(document).ready(onReady);
 ////   EVENTS
 function onReady(){
 
+    $('#equalBtn').on('submit', onPostEquation);
+
 
 };
 
@@ -17,7 +19,32 @@ function onReady(){
 
 ///    UPDATE STATE   ---   AJAX GET/POST
 
+function onPostEquation(evt){
+    evt.preventDefault();
 
+    let equation = {
+        num1: $('#firstNum').val(),
+        // operation: //HOW TO DETERMINE WHICH OPERATION WAS SELECTED
+        num2: $('#secNum').val()
+    };
+
+    console.log(equation);
+
+    $.ajax({
+        url: '/equations',
+        method: 'POST',
+        data: equation
+    })
+    .then(response=>{
+        console.log('here in postEquation', response);
+    })
+    .catch(err=>{
+        console.log('Oops! Error: ', err);
+    });
+    
+
+
+}
 
 
 
--- server/server.js
+++ server/server.js
@@ -9,6 +9,40 @@ app.use(bodyParser.urlencoded({extended:true}));
 //Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
+console.log('here in the server');
+
+
+
+let currentCalculation
+let allCalculations= [];
+
+
+
+
+app.post('/equations', (req, res)=>{
+
+    currentCalculation= req.body;
+
+     console.log(req.body);
+     allCalculations.push(currentCalculation);
+
+    ///⬇️ IF NOT INCLUDED THE PAGE WILL NEED TO BE REFRESHED TO SEE NEW INFO
+    res.sendStatus(201);
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
