d79b438e577a3ce58d8f8151fed498bb8e7de654
Crystal Redmon <cato2848@gmail.com>
2022-09-29 21:01:01 -0500

    client.js onReady established

---
+++ server/public/client.js
@@ -0,0 +1,28 @@
+console.log('in client.js');
+
+$(document).ready(onReady);
+
+////   STATE/GLOBAL VARIABLES
+
+
+
+////   EVENTS
+function onReady(){
+
+
+};
+
+
+
+
+///    UPDATE STATE   ---   AJAX GET/POST
+
+
+
+
+
+
+
+
+
+///    RENDER TO DOM
