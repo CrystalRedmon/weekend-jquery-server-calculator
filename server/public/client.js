457912e81413fa39e780622c232c97ac839a9d43
Crystal Redmon <cato2848@gmail.com>
2022-09-30 12:03:06 -0500

    create onAddNum function

--- server/public/client.js
+++ server/public/client.js
@@ -5,9 +5,15 @@ $(document).ready(onReady);
 ////   STATE/GLOBAL VARIABLES
 
 
+///UNSURE IF THIS NEEDS TO BE GLOBAL
+let clientCurrentEquation = []
+
 
 ////   EVENTS
 function onReady(){
+
+    ///  listener for each operation
+    $('#add').on('click', onAddNum)
 
     $('#equalBtn').on('submit', onPostEquation);
 
@@ -15,36 +21,77 @@ function onReady(){
 };
 
 
-
-
-///    UPDATE STATE   ---   AJAX GET/POST
-
-function onPostEquation(evt){
-    evt.preventDefault();
-
-    let equation = {
-        num1: $('#firstNum').val(),
-        // operation: //HOW TO DETERMINE WHICH OPERATION WAS SELECTED
-        num2: $('#secNum').val()
+function onAddNum(){
+    let addNumbers ={
+        firstNum: $('#firstNum'),
+        addOperation: '+',
+        secNum: $('#secNum')
+        
     };
 
-    console.log(equation);
+    clientCurrentEquation = addNumbers
 
     $.ajax({
-        url: '/equations',
+        url: '/add',
         method: 'POST',
-        data: equation
+        data: addNumbers
     })
     .then(response=>{
-        console.log('here in postEquation', response);
+        console.log('in add ajax POST');
+
+        ///THIS CALLS FUNCTION THAT WILL 'GET' EQUATION HISTORY
+        getStoredEquations();
+
+        ///THIS CALLS THE FUNCTION THAT WILL GET CURRENT RESULT
+        getCurrentResult();
     })
-    .catch(err=>{
-        console.log('Oops! Error: ', err);
-    });
+
+};
+
+
+
+
+
+//     UPDATE STATE   ---   AJAX GET/POST
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function onPostEquation(evt){
+//     evt.preventDefault();
+
+//     let equation = {
+//         num1: $('#firstNum').val(),
+//         // operation: //HOW TO DETERMINE WHICH OPERATION WAS SELECTED
+//         num2: $('#secNum').val()
+//     };
+
+//     console.log(equation);
+
+//     $.ajax({
+//         url: '/equations',
+//         method: 'POST',
+//         data: equation
+//     })
+//     .then(response=>{
+//         console.log('here in postEquation', response);
+//     })
+//     .catch(err=>{
+//         console.log('Oops! Error: ', err);
+//     });
     
 
 
-}
+// }
 
 
 
@@ -52,4 +99,4 @@ function onPostEquation(evt){
 
 
 
-///    RENDER TO DOM
+// ///    RENDER TO DOM
