304756dbde63160065e4c5f209ff04e10d26ef91
Crystal Redmon <cato2848@gmail.com>
2022-10-01 13:13:36 -0500

    complete serverCalculationResult

--- server/server.js
+++ server/server.js
@@ -26,30 +26,12 @@ app.post('/calculate', (req, res)=>{
     currentCalculation = req.body;
     allCalculations.push(currentCalculation);
 
-    // console.log(currentCalculation);
-    // console.log(allCalculations)
+    console.log(currentCalculation);
+    console.log(allCalculations)
 
     ///⬇️ IF NOT INCLUDED THE PAGE WILL NEED TO BE REFRESHED TO SEE NEW INFO
     res.sendStatus(201);
  
-    
-console.log(currentCalculation);
-
-// console.log(concat(Number(currentCalculation.firstNum), currentCalculation.operation, Number(currentCalculation.secNum)));
-
-
-
-
-// let firstNum = Number(currentCalculation.firstNum);
-// let secNum = Number(currentCalculation.secNum);
-// let operation = currentCalculation.operation
-
-
-
-// let tryCal =  lksdfjla.concat('+', secNum);
-// console.log(tryCal);
-
-// let tryCal = firstNum operation secNum ;
 
 console.log(serverCalculationResult());
 
@@ -60,12 +42,25 @@ console.log(serverCalculationResult());
 
 
 
+
+
 //////////  UPDATE STATE   ////////
 
+
+///////// GET will call this funtion to retrieve the current result
 function serverCalculationResult(){
 
     if(currentCalculation.operation === '+'){
         let result = Number(currentCalculation.firstNum) + Number(currentCalculation.secNum);
+        return result;
+    }else if(currentCalculation.operation === '-'){
+        let result = Number(currentCalculation.firstNum) - Number(currentCalculation.secNum);
+        return result; 
+    }else if(currentCalculation.operation === '*'){
+        let result = Number(currentCalculation.firstNum) * Number(currentCalculation.secNum);
+        return result;
+    }else if(currentCalculation.operation === '/'){
+        let result = Number(currentCalculation.firstNum) / Number(currentCalculation.secNum);
         return result;
     }
 
